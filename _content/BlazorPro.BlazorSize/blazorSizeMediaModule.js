381d54b97d7b31ef2a72485f66ff3ea26b4ee28b
alexandrelozano <alexandre.lozano@gmail.com>
2021-01-06 08:20:17 +0000

    Deploying to gh-pages from  @ 768f1944ab889f99e828960beb86734b8d88c25d 🚀

---
+++ _content/BlazorPro.BlazorSize/blazorSizeMediaModule.js
@@ -0,0 +1,22 @@
+import { BlazorSizeMedia } from './blazorSizeMedia.js';
+let instance = new BlazorSizeMedia();
+// When MQL is created, it tracks a NET Ref
+export function addMediaQueryList(dotnet) {
+    instance.addMediaQueryList(dotnet);
+}
+// OnDispose this method cleans the MQL instance
+export function removeMediaQueryList(dotnetMql) {
+    instance.removeMediaQueryList(dotnetMql);
+}
+// When MediaQuery instance is created track media query in JavaScript
+export function addMediaQueryToList(dotnetMql, mediaQuery) {
+    return instance.addMediaQueryToList(dotnetMql, mediaQuery);
+}
+// When MediaQuery instance is disposed remove media query in JavaScript
+export function removeMediaQuery(dotnetMql, mediaQuery) {
+    instance.removeMediaQuery(dotnetMql, mediaQuery);
+}
+// Get media query value for cache after app loads
+export function getMediaQueryArgs(mediaQuery) {
+    return instance.getMediaQueryArgs(mediaQuery);
+}
